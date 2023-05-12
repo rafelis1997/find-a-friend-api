9ca6f1a46e832af8ac2abcceb3252d2406ff1d8b
rafelis1997 <rafaelheros80@gmail.com>
2023-05-12 14:33:54 -0300

    chore: add verify org role middleware

--- src/http/controllers/pets/routes.ts
+++ src/http/controllers/pets/routes.ts
@@ -4,6 +4,7 @@ import { registerPet } from './register'
 import { VerifyJwt } from '@/http/middleware/verify-jwt'
 import { petDetails } from './pet-details'
 import { listPets } from './list-pets'
+import { verifyUserRole } from '@/http/middleware/verify-roles'
 
 export async function petsRoutes(app: FastifyInstance) {
   app.get('/pets/:city', listPets)
@@ -11,5 +12,9 @@ export async function petsRoutes(app: FastifyInstance) {
   app.get('/pets/details/:petId', petDetails)
 
   /* Authenticated **/
-  app.post('/pets/register', { onRequest: [VerifyJwt] }, registerPet)
+  app.post(
+    '/pets/register',
+    { onRequest: [VerifyJwt, verifyUserRole('ADMIN')] },
+    registerPet,
+  )
 }
